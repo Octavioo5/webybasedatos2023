5bebb9e470503a1e0d21bc38cd0dc03481b21eaa
Octavioo5 <124410741+Octavioo5@users.noreply.github.com>
2023-09-11 19:06:04 -0700

    Delete practica3.js

--- practica3.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var contador=0;
-    $('img').click(function (e){
-        var nombre_img = $(this).attr('data-id')
-        $(this).attr('src','fotos/'+ nombre_img);
-    });
-});
