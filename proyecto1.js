f316f9496bc4e6f383322e97682557b5daad461f
Octavioo5 <124410741+Octavioo5@users.noreply.github.com>
2023-09-24 11:01:25 -0700

    24/09/2023

--- proyecto1.js
+++ proyecto1.js
@@ -1,44 +1,35 @@
-//cargo en un arreglo las imganes de las banderas. Este sera el orden que se mostrarán
-let banderas = ["mx.png", "br.png", "ar.png"];
 
-//arreglo que guardara la opcion correcta
-let correcta = [2,1,0];
-
-//arreglo que guardara los paises a mostrar en cada jugada
+let banderas = ["mx.png", "br.png", "ar.png", "kr.png", "de.png"];
+let correcta = [2,1,0,2,1];
 let opciones = [];
-//cargo en el arreglo opciones las opciones a mostrar en cada jugada
+
+ 
 opciones.push(["ESCOCIA", "SINGAPUR", "MEXICO"]);
 opciones.push(["ESPAÑA", "BRASIL", "BOLIVIA"]);
 opciones.push(["ARGENTINA", "ANDORRA", "GALES"]);
+opciones.push(["CANADA", "REINO UNIDO", "COREA DEL SUR"]);
+opciones.push(["HONDURAS", "ALEMANIA", "CHINA"]); 
 
 
-//variable que guarda la posicion actual
 let posActual = 0;
-//variable que guarda la cantidad acertadas hasta el moemento
 let cantidadAcertadas = 0;
+let banderas_jugadas=0;
 
 function comenzarJuego(){
-    //reseteamos las variables
+
     posActual = 0;
     cantidadAcertadas = 0;
-    //activamos las pantallas necesarias
     document.getElementById("pantalla-inicial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "block";
     cargarBandera();
 
 }
 
-//funcion que carga la siguiente bandera y sus opciones
-function cargarBandera(){
-    $("#pregunta1").attr("visibility","visible") 
-    $("#pregunta2").attr("visibility","visible") 
-    $("#pregunta3").attr("visibility","visible") 
-    //controlo sis se acabaron las banderas
-    if(banderas.length <= posActual){
+        function cargarBandera(){
+        if(banderas.length <= posActual){
         terminarJuego();
     }
-    else{//cargo las opciones
-        //limpiamos las clases que se asignaron
+        else{
         limpiarOpciones();
 
         document.getElementById("imgBandera").src = "fotos/" + banderas[posActual];
@@ -46,78 +37,44 @@ function cargarBandera(){
         document.getElementById("n1").innerHTML = opciones[posActual][1];
         document.getElementById("n2").innerHTML = opciones[posActual][2];
     }
-  
 }
+        function limpiarOpciones(){
+        document.getElementById("n0").className = "nombre";
+        document.getElementById("n1").className = "nombre";
+        document.getElementById("n2").className = "nombre";
 
-function limpiarOpciones(){
-    document.getElementById("n0").className = "nombre";
-    document.getElementById("n1").className = "nombre";
-    document.getElementById("n2").className = "nombre";
+        document.getElementById("l0").className = "letra";
+        document.getElementById("l1").className = "letra";
+        document.getElementById("l2").className = "letra";
+        }
 
-    document.getElementById("l0").className = "letra";
-    document.getElementById("l1").className = "letra";
-    document.getElementById("l2").className = "letra";
-}
-
-function comprobarRespuesta(opElegida){
-    console.log(opElegida)
-    if (opElegida==undefined) {
-        opElegida=3
-        console.log("fkjshf")
-        document.getElementById("n" + opElegida).className = "nombre nombreNoAcertada";
-        document.getElementById("l" + opElegida).className = "letra letraNoAcertada";
-
-        //opcion que era correcta
-        document.getElementById("n" + correcta[posActual]).className = "nombre nombreAcertada";
-        document.getElementById("l" + correcta[posActual]).className = "letra letraAcertada";
-        setTimeout(cargarBandera,1000);
-        return;
-    }
-    if(opElegida==correcta[posActual]){//acertó
-        //agregamos las clases para colocar el color verde a la opcion elegida
+        function comprobarRespuesta(opElegida){
+         if(opElegida==correcta[posActual]){
+       
         document.getElementById("n" + opElegida).className = "nombre nombreAcertada";
         document.getElementById("l" + opElegida).className = "letra letraAcertada";
         cantidadAcertadas++;
-    }else{//no acerto
-        //agramos las clases para colocar en rojo la opcion elegida
+        }else{
         document.getElementById("n" + opElegida).className = "nombre nombreNoAcertada";
         document.getElementById("l" + opElegida).className = "letra letraNoAcertada";
 
-        //opcion que era correcta
+       
         document.getElementById("n" + correcta[posActual]).className = "nombre nombreAcertada";
         document.getElementById("l" + correcta[posActual]).className = "letra letraAcertada";
-    }
+        }
+
     posActual++;
-    //Esperamos 1 segundo y pasamos mostrar la siguiente bandera y sus opciones
     setTimeout(cargarBandera,1000);
-}
-
-var i= 5;
-setInterval(function(){
-    if (i==0) {
-        i=5
-        alert("termino")
-        setTimeout(comprobarRespuesta,1000);
     }
-    i--;
-},1000);
+    function terminarJuego(){
+        document.getElementById("pantalla-juego").style.display = "none";
+        document.getElementById("pantalla-final").style.display = "block";
+        document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
+        document.getElementById("numIncorrectas").innerHTML = banderas.length - cantidadAcertadas;
+    }
 
-
-function terminarJuego(){
-    //ocultamos las pantallas y mostramos la pantalla final
-    document.getElementById("pantalla-juego").style.display = "none";
-    document.getElementById("pantalla-final").style.display = "block";
-    //agreamos los resultados
-    document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
-    document.getElementById("numIncorrectas").innerHTML = banderas.length - cantidadAcertadas;
+    function volverAlInicio(){
+        document.getElementById("pantalla-final").style.display = "none";
+        document.getElementById("pantalla-inicial").style.display = "block";
+        document.getElementById("pantalla-juego").style.display = "none";
 }
-
-function volverAlInicio(){
-    //ocultamos las pantallas y activamos la inicial
-    document.getElementById("pantalla-final").style.display = "none";
-    document.getElementById("pantalla-inicial").style.display = "block";
-    document.getElementById("pantalla-juego").style.display = "none";
-}
-
-
-
